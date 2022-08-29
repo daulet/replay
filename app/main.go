71f165e67dc3712ee54534baa35cf4b2c987c3e9
Daulet Zhanguzin <daulet@cohere.com>
2022-08-28 23:18:45 -0700

    wrap all parsing into one connection

--- app/main.go
+++ app/main.go
@@ -10,19 +10,19 @@ import (
 	"github.com/daulet/redisreplay"
 )
 
-// $ go run app/main.go --port 8080 --dest localhost:6379
+// $ go run app/main.go --port 8080 --target localhost:6379
 // $ redis-cli -p 8080
 
 func main() {
-	dest := flag.String("dest", "", "destination host:port")
+	target := flag.String("target", "", "target host:port")
 	port := flag.Int("port", 0, "port to listen on")
 	flag.Parse()
 
-	target, err := net.Dial("tcp", *dest)
+	dst, err := net.Dial("tcp", *target)
 	if err != nil {
 		panic(err)
 	}
-	defer target.Close()
+	defer dst.Close()
 
 	lstr, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
@@ -35,21 +35,20 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		conn = redisreplay.NewRecorder(conn.(*net.TCPConn), "response: ")
-		target = redisreplay.NewRecorder(target.(*net.TCPConn), "request: ")
-		go handle(conn, target)
+		src := redisreplay.NewRecorder(conn.(*net.TCPConn))
+		go handle(src, dst)
 	}
 }
 
-func handle(in net.Conn, out net.Conn) {
-	defer in.Close()
+func handle(src io.ReadWriteCloser, dst io.ReadWriter) {
+	defer src.Close()
 
 	go func() {
-		if _, err := io.Copy(out, in); err != nil {
+		if _, err := io.Copy(dst, src); err != nil {
 			log.Printf("write from in to out: %v", err)
 		}
 	}()
-	if _, err := io.Copy(in, out); err != nil {
+	if _, err := io.Copy(src, dst); err != nil {
 		log.Printf("write from out to in: %v", err)
 	}
 }
--- recorder.go
+++ recorder.go
@@ -2,9 +2,15 @@ package redisreplay
 
 import (
 	"io"
+	"io/ioutil"
 	"net"
 	"os"
 )
+
+// hide the TCPConn methods we don't want to expose, e.g. ReadFrom
+type readWriteCloserOnly struct {
+	io.ReadWriteCloser
+}
 
 // TODO need to differentiate between request and response callers
 type Recorder struct {
@@ -18,35 +24,39 @@ type Recorder struct {
 	reqID int
 }
 
-// prefix is prefixed to all responses
-func NewRecorder(conn *net.TCPConn, prefix string) *Recorder {
+func NewRecorder(conn *net.TCPConn) io.ReadWriteCloser {
 	writer := os.Stdout
-	resTee := NewTeeWriter(conn, os.Stdout, prefix)
+	reqTee := NewTeeWriter(ioutil.Discard, writer, "request: ")
+	resTee := NewTeeWriter(conn, writer, "response: ")
 
-	return &Recorder{
-		TCPConn: *conn,
-		writer:  writer,
-		reqID:   0,
+	return readWriteCloserOnly{
+		&Recorder{
+			TCPConn: *conn,
+			writer:  writer,
+			reqID:   0,
 
-		resTee: resTee,
+			reqTee: reqTee,
+			resTee: resTee,
+		},
 	}
 }
 
 var _ net.Conn = (*Recorder)(nil)
 
 func (r *Recorder) ReadFrom(rdr io.Reader) (int64, error) {
-	n, err := io.Copy(r.resTee, rdr)
-	if err != nil {
-		return n, err
-	}
-	return n, nil
+	return io.Copy(r.reqTee, rdr)
 }
 
-// TODO implement WriteTo
-func (r *Recorder) Write(b []byte) (int, error) {
-	return r.reqTee.Write(b)
+func (r *Recorder) Read(p []byte) (int, error) {
+	n, err := r.TCPConn.Read(p)
+	r.reqTee.Write(p[:n])
+	return n, err
+}
+
+func (r *Recorder) Write(p []byte) (int, error) {
+	return r.resTee.Write(p)
 }
 
 func (r *Recorder) Close() error {
-	return nil
+	return r.TCPConn.Close()
 }
